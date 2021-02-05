9b15128a71bead755e10ffd8e2997f3994d2faba
Niraj-Kulkarni <78420588+Niraj-Kulkarni@users.noreply.github.com>
2021-02-05 18:41:38 +0530

    Update and rename HW2.py to Day2_HW_Problem2.py

---
+++ Homeworks/Day2_HW_Problem2.py
@@ -0,0 +1,5 @@
+# Taking user input #
+n=int(input("Enter a single digit number: "))
+for i in range(0,n+1,2): #Creating a for loop with a step of 2 to iterate # 
+    print(i) # Printing all the even numbers #
+
